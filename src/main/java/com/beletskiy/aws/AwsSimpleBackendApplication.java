40ba4526ae6931cd90f317d08fb0971fef6c661a
vibe <vitaliy.beletskiy@gmail.com>
2026-04-02 14:09:48 +0200

    add basic endpoint

--- src/main/java/com/beletskiy/aws/AwsSimpleBackendApplication.java
+++ src/main/java/com/beletskiy/aws/AwsSimpleBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsSImpleBackendApplication {
+public class AwsSimpleBackendApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AwsSImpleBackendApplication.class, args);
+    SpringApplication.run(AwsSimpleBackendApplication.class, args);
   }
 }
---
+++ src/main/java/com/beletskiy/aws/HelloController.java
@@ -0,0 +1,15 @@
+package com.beletskiy.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public Map<String, String> home() {
+    return Map.of("message", "Backend is running");
+  }
+}
