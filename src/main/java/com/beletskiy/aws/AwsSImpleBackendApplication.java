e2f212b1e43b8893945a039d7d9653e631461fd3
vibe <vitaliy.beletskiy@gmail.com>
2026-04-02 14:00:48 +0200

    initial commit

---
+++ src/main/java/com/beletskiy/aws/AwsSImpleBackendApplication.java
@@ -0,0 +1,12 @@
+package com.beletskiy.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSImpleBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsSImpleBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/beletskiy/aws/AwsSImpleBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.beletskiy.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSImpleBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
