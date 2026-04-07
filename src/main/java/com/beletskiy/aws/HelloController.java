35663fff572888b248827f55bcd6b4316a43e342
vibe <vitaliy.beletskiy@gmail.com>
2026-04-07 13:18:31 +0200

    add timestamp to server response

--- src/main/java/com/beletskiy/aws/HelloController.java
+++ src/main/java/com/beletskiy/aws/HelloController.java
@@ -3,6 +3,9 @@ package com.beletskiy.aws;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @RestController
@@ -10,6 +13,8 @@ public class HelloController {
 
   @GetMapping("/")
   public Map<String, String> home() {
-    return Map.of("message", "Backend is running");
+    ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/Stockholm"));
+    String formatted = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    return Map.of("message", "Backend is running", "timestamp", formatted);
   }
 }
